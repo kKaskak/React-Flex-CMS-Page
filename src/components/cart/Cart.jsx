68b8801b934456c5219ce84c2e2dced369d3d363
kKaskak <117831817+kKaskak@users.noreply.github.com>
2024-01-20 20:05:40 +0200

    refactor: code styles

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -40,50 +40,50 @@ const Cart = () => {
 	const handleCheckout = 'fdds';
 
 	return (
-		<div className="cart-wrapper" ref={cartRef}>
-			<div className="cart-container">
+		<div className='cart-wrapper' ref={cartRef}>
+			<div className='cart-container'>
 				<button
-					type="button"
-					className="cart-heading"
+					type='button'
+					className='cart-heading'
 					onClick={() => setShowCart(false)}
 				>
 					<AiOutlineLeft />
-					<span className="heading">Your Cart</span>
-					<span className="cart-num-items">({totalQuantities} items)</span>
+					<span className='heading'>Your Cart</span>
+					<span className='cart-num-items'>({totalQuantities} items)</span>
 				</button>
 				{cartItems.length < 1 && (
-					<div className="empty-cart">
+					<div className='empty-cart'>
 						<AiOutlineShopping size={150} />
 						<h3>Your shopping bag is empty</h3>
-						<Link to="/pricing"><button type="button" onClick={() => setShowCart(false)} className="btn">Continue Shopping</button></Link>
+						<Link to='/pricing'><button type='button' onClick={() => setShowCart(false)} className='btn'>Continue Shopping</button></Link>
 					</div>
 				)}
-				<div className="product-container">
+				<div className='product-container'>
 					{cartItems.length >= 1 &&
 						cartItems.map((item) => (
-							<div className="product" key={item._id}>
-								<img src={urlFor(item?.image[0])} className="cart-product-image"/>
-								<div className="item-desc">
-									<div className="flex top">
+							<div className='product' key={item._id}>
+								<img src={urlFor(item?.image[0])} className='cart-product-image'/>
+								<div className='item-desc'>
+									<div className='flex top'>
 										<h5>{item.name}</h5>
 										<h4>${item.price}</h4>
 									</div>
-									<div className="flex bottom">
+									<div className='flex bottom'>
 										<div>
-											<p className="quantity-desc">
+											<p className='quantity-desc'>
 												<span
-													className="minus"
+													className='minus'
 													onClick={() =>
 														toggleCartItemQuantity(item._id, 'dec')
 													}
 												>
 													<AiOutlineMinus />
 												</span>
-												<span className="num" onClick="">
+												<span className='num' onClick=''>
 													{item.quantity}
 												</span>
 												<span
-													className="plus"
+													className='plus'
 													onClick={() =>
 														toggleCartItemQuantity(item._id, 'inc')
 													}
@@ -93,8 +93,8 @@ const Cart = () => {
 											</p>
 										</div>
 										<button
-											type="button"
-											className="remove-item"
+											type='button'
+											className='remove-item'
 											onClick={() => onRemove(item)}
 										>
 											<TiDeleteOutline />
@@ -105,13 +105,13 @@ const Cart = () => {
 						))}
 				</div>
 				{cartItems.length >= 1 && (
-					<div className="cart-bottom">
-						<div className="total">
+					<div className='cart-bottom'>
+						<div className='total'>
 							<h3>Subtotal:</h3>
 							<h3>${totalPrice.toFixed(2)}</h3>
 						</div>
-						<div className="btn-container">
-							<button type="button" className="btn" onClick={handleCheckout}>Pay with Stripe</button>
+						<div className='btn-container'>
+							<button type='button' className='btn' onClick={handleCheckout}>Pay with Stripe</button>
 						</div>
 					</div>
 				)}
